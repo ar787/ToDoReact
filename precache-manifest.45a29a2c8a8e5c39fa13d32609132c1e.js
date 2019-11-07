47c9fd3a82bd587a30f1a717e54c1a0c99b11737
ar787 <armanhovhannisyan1002@gmail.com>
2019-11-07 18:43:04 +0400

    Updates

---
+++ precache-manifest.45a29a2c8a8e5c39fa13d32609132c1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae3855034659680b8e3c69640674d390",
+    "url": "/ToDoReact/index.html"
+  },
+  {
+    "revision": "9301ce145e1a91949eef",
+    "url": "/ToDoReact/static/css/main.5ef96ef4.chunk.css"
+  },
+  {
+    "revision": "c467d638a960f2f95360",
+    "url": "/ToDoReact/static/js/2.8af3b707.chunk.js"
+  },
+  {
+    "revision": "9301ce145e1a91949eef",
+    "url": "/ToDoReact/static/js/main.64849686.chunk.js"
+  },
+  {
+    "revision": "1e2d8881280807f37420",
+    "url": "/ToDoReact/static/js/runtime-main.d21b9e9a.js"
+  }
+]);
