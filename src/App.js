7c388f0d30dde120d952aead0dde671190bc72c9
ar787 <53258333+ar787@users.noreply.github.com>
2019-11-07 19:08:21 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-// import { withStyles } from "@material-ui/core/styles";
 import './App.css';
 import {ToDo} from "./myComponents/ToDo";
-// import Demo from "../src/hoc/returnButton";
+
 function App() {
   
   return (
